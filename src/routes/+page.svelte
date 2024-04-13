9ba8797cee7bf196df265fd8e4bdeb744291aae7
Aline <yejilines@gmail.com>
2024-04-13 18:27:57 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let text = "8=D"
+    const crescer = () => {
+        text = text.slice(0, text.length-1) + "=D"
+    }
+</script>
+
+<svelte:head>
+    <title> Yejiverso </title>
+</svelte:head>
+<h1>Pinto</h1>
+<p>{text}</p>
+<button on:click={crescer}> Fazer o pinto crescer </button>
