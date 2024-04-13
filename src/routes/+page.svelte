822a084ba007b3b10c2ce1d10ee46b752883a9d0
Aline <yejilines@gmail.com>
2024-04-13 20:24:12 -0300

    jmk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,17 @@
             Apareceu pela primeira vez em agosto de 2016 no "JYP showcase". Em 2017, juntamente com as futuras integrantes de seu atual grupo (exceto Lia), apareceu no reality show da Mnet, Stray Kids, como parte da equipe feminina de pré-debut da JYP Entertainment. No final de 2018, ela se tornou participante do programa de sobrevivência "The Fan" da SBS, mas desistiu sem marcar pontos suficientes.
         </p>
         </div>
-<style>
+<style lang="scss">
     @font-face {
         font-family: sedan;
         src: url("/Sedan-Regular.ttf")
     }
    .jmk{
         font-family: sedan;
+        font-size: large;
         padding: 1vh 1vw;
         margin: 25vh auto;
-        height: 39vh; 
+        height: 45vh; 
         width: 65vh;
         border-style: solid;
         box-shadow: 5px 5px #ff748c, 10px 10px #ff8da1, 15px 15px #ffa7b6, 20px 20px #ffdae0;
